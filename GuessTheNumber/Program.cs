c047de405e97b52d73b09d670b81490be368390f
Nagesh04 <62748296+Nagesh04@users.noreply.github.com>
2020-04-01 18:43:30 +0530

    #First Commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t<<<-Guess The Number You Have Four Chances->>>");
+            Random A = new Random();
+            var radno = A.Next(1, 10);
+
+            int i = 1;
+            while (i <= 4)
+            {
+                int flag = 0;
+
+                Console.WriteLine("<<<-Enter no->>>");
+                var input = Console.ReadLine();
+                var input1 = Convert.ToInt32(input);
+                if (input1 == radno)
+                {
+                    Console.WriteLine("\t\t\t\t<<<-You Guessed The Correct->>>");
+                    Console.WriteLine("\t\t\t\t*****YOU WON THE GAME*****");
+                    flag = 1;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\t\t\t\t<<<-You Guessed Wrong->>>");
+
+                }
+                var chance = (4 - i);
+                Console.WriteLine("\t\t\t\t<<<-You Have {0} Chances Left->>>", chance);
+                if (chance == 0 && flag == 0)
+                {
+                    Console.WriteLine("\t\t\t\t<<<-You Lost The Game->>>");
+                }
+                //Console.WriteLine(A);
+                //Console.WriteLine(radno);
+                i += 1;
+
+
+            }
+        }
+
+    }
+}
---
+++ UniqueNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            int i = 0;
+            while (i <= 5)
+            {
+
+                Console.WriteLine("<<<--Enter The Five Unique Numbers-->>>");
+                var input = Console.ReadLine();
+                var input1 = Convert.ToInt32(input);
+
+                if (list.Contains(input1))
+                {
+                    Console.WriteLine("Error Already exist");
+                }
+                else
+                {
+                    list.Add(input1);
+                    i += 1;
+                    if (i == 5)
+                    {
+                        Console.WriteLine("Unique Numbers:-");
+                        foreach (var content in list)
+                        {
+                            Console.WriteLine(content);
+                        }
+                        break;
+                    }
+                }
+
+
+
+            }
+        }
+    }
+}
